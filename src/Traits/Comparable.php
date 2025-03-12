e6ab244ff57e4ca4a1815da9c3c03918c13ec038
brann-meius <bohdan.meius@nixs.com>
2025-03-12 10:22:16 +0100

    feature/v1.0.1#dependencyOptimization: Removed unused dependencies. Improved trait annotation.

--- src/Traits/Comparable.php
+++ src/Traits/Comparable.php
@@ -5,7 +5,16 @@ declare(strict_types=1);
 namespace Meius\FlagForge\Traits;
 
 use Meius\FlagForge\Contracts\Bitwiseable;
+use Meius\FlagForge\FlagManager;
 
+/**
+ * Provides default methods for comparing bitwise flags.
+ *
+ * The `has` method checks if a given flag is set in the object's mask,
+ * while `doesntHave` returns the inverse result.
+ *
+ * @mixin FlagManager
+ */
 trait Comparable
 {
     public function has(Bitwiseable $flag): bool
